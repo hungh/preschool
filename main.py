ca70d66c3b3768da73d1d98164e48390fa1be932
h.huynh <h.huynh@tmcbonds.com>
2014-12-11 16:50:53 -0500

    Testing model in mongoengine

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from global_const import KEY_LOGIN, KEY_PASSWORD, DEBUG_MODE, WRONG_CREDENTIAL
 from db_config import DB_HOST, DB_PORT, DB_NAME
-from flask import Flask, render_template, request, g
-from mongoengine import connect
+from flask import Flask, render_template, request
+from models import User, Guest, SpellEntry, MathEntry, SpellAnswer, MathAnswer
+from mongoengine import connect, errors
 
 app = Flask(__name__)
 app.config.from_pyfile('db_config.py')
-g.db = connect(DB_NAME, host=DB_HOST, port=DB_PORT)
+db = connect(DB_NAME, host=DB_HOST, port=DB_PORT)
 
 
 @app.route('/')
@@ -21,6 +22,36 @@ def login_access():
         return render_template('work.html', msg=login)
     else:
         return render_template("index.html", msg=WRONG_CREDENTIAL)
+
+
+@app.route("/test")
+def test_db():
+
+
+    #admin = User(login="admin", hash_string="GHAFFASHDALSDHASJDA:DA877")
+    #guest = Guest(login="guest", hash_string="FHSDADKANDAKSDASD", name="guest_name")
+    #spell_entry = SpellEntry(sid=1, image_name="apple.png",
+    #                            array_letters=['a', 's', 'p', 'p', 'k', 'l', 'e'], answer="apple", level=1)
+    math_entry = MathEntry(mid=1, expression="2 + 3", level=1)
+
+
+
+
+    #spell_answer = SpellAnswer(owner=guest, spell_entry=spell_entry, answer="applk")
+    #spell_answer.save()
+    #msg = "Saving spell answer"
+    for spell_entry in SpellEntry.objects(sid='1'):
+        res = spell_entry
+        break
+
+    for user in Guest.objects(name="guest_name"):
+        guest_name = user
+        break
+
+    spell_answer = SpellAnswer(owner=guest_name, spell_entry=spell_entry, answer="applk")
+    spell_answer.save()
+
+    return res.image_name
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -5,6 +5,12 @@ class User(Document):
     login = StringField(max_length=50, required=True, unique=True)
     hash_string = StringField(max_length=61, required=True)
 
+    meta = {'allow_inheritance': True}
+
+
+class Guest(User):
+    name = StringField(required=True)
+
 
 class SpellEntry(Document):
     sid = FloatField(min_value=1, unique=True)
@@ -20,17 +26,13 @@ class MathEntry(Document):
     level = IntField(min_value=1, max_value=9)
 
 
-class SpellAnswer(EmbeddedDocument):
-    spell_entry = ReferenceField(SpellEntry, required=True)
+class SpellAnswer(Document):
+    owner = ReferenceField(Guest, reverse_delete_rule=CASCADE)
+    spell_entry = ReferenceField(SpellEntry, required=True, reverse_delete_rule=CASCADE)
     answer = StringField()
 
 
-class MathAnswer(EmbeddedDocument):
-    math_entry = ReferenceField(MathEntry, required=True)
+class MathAnswer(Document):
+    owner = ReferenceField(Guest, reverse_delete_rule=CASCADE)
+    math_entry = ReferenceField(MathEntry, required=True, reverse_delete_rule=CASCADE)
     answer = StringField()
-
-
-class Guest(Document):
-    name = StringField(required=True)
-    spell_entries = ListField(EmbeddedDocumentField(SpellEntry))
-    math_entries = ListField(EmbeddedDocumentField(MathAnswer))
